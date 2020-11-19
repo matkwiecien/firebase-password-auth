c3433f68dcb7db6bbdb46e4743d4c4880c04f8cd
Mateusz Kwiecien <m.kwiecien916@gmail.com>
2020-11-19 06:54:56 +0100

    Fix IonButton mock

--- src/mocks/ionic/IonButtonMock.tsx
+++ src/mocks/ionic/IonButtonMock.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const IonButtonMock: React.FC<any> = ({children, ...props}) => {
-    return <button {...props}>{children}</button>
+const IonButtonMock: React.FC<any> = ({children, type='button', ...props}) => {
+    return <button type={type} {...props}>{children}</button>
 }
 
 export default IonButtonMock;
