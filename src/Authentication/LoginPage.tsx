971bc50f7be61d8316291af3fb8bf062dfe1e378
Mateusz Kwiecien <m.kwiecien916@gmail.com>
2020-11-19 06:41:40 +0100

    Add formik to manage form state

--- src/Authentication/LoginPage.tsx
+++ src/Authentication/LoginPage.tsx
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
+import { useFormik } from 'formik';
 
 import { IonPage, IonContent, IonItem, IonInput, IonLabel, IonButton, IonLoading } from '@ionic/react';
 
 const LoginPage: React.FC = () => {
     const [busy, setBusy] = useState(false);
+    const { values, handleSubmit, setFieldValue } = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        onSubmit: values => {
+            setBusy(true);
+        }
+    });
 
     return (
         <IonPage>
             <IonContent>
                 <h1>Sign in</h1>
-                <form onSubmit={() => {
-                    setBusy(true)
-                }}>
+                <form onSubmit={handleSubmit}>
                     <IonItem>
                         <IonLabel>Email: </IonLabel>
                         <IonInput
                             data-testid='signin-email-input'
                             type='email'
                             name='email'
+                            value={values.email}
+                            onIonChange={(evt) => setFieldValue('email', evt.detail.value)}
                         />
                     </IonItem>
                     <IonItem>
@@ -26,6 +36,8 @@ const LoginPage: React.FC = () => {
                             data-testid='signin-password-input'
                             type='password'
                             name='password'
+                            value={values.password}
+                            onIonChange={(evt) => setFieldValue('password', evt.detail.value)}
                         />
                     </IonItem>
                     <IonButton>Sign In</IonButton>
